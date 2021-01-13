6a2956dda1108ebba74e5f49af0fc279df405ee8
Mathias Nikkel <mathiasnikkel@gmail.com>
2021-01-12 23:48:38 -0300

    s3 config

--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -1,8 +1,15 @@
 import { container } from 'tsyringe';
+import uploadsConfrig from '@config/upload';
 import DiskStorageProvider from './implementations/DiskStorageProvider';
+import S3StorageProvider from './implementations/S3StorageProvider';
 import IStorageProvider from './models/IStorageProvider';
+
+const providers = {
+  disk: DiskStorageProvider,
+  s3: S3StorageProvider,
+};
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
-  DiskStorageProvider,
+  providers[uploadsConfrig.driver],
 );
